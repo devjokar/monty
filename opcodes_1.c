a449c92890abaa965cd162793273c2780270b7f7
Jo Kar <josi.kelechi@gmail.com>
2022-12-18 19:08:23 -0800

    Refactor code

--- opcodes_1.c
+++ opcodes_1.c
@@ -36,9 +36,8 @@ void op_push(stack_t **head, unsigned int counter)
 		if (bus.arg[0] == '-')
 			j++;
 		for (; bus.arg[j] != '\0'; j++)
-		{
 			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
+				flag = 1;
 		if (flag == 1)
 		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
